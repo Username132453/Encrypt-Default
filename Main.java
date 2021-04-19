c8480694274fed7c4b7857da8f918f47b7c4d181
Brett Dynek <>
2021-04-19 16:27:30 +0000

    Made Base Implementation
    ToDo:
     -Generate Key
     -Make Key Make
     new text

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 class Main {
+  //Make our Console Scanner
+  Scanner console = new Scanner(system.in);
   public static void main(String[] args) {
-    
+
   }
 }
